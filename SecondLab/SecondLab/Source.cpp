8fab20a9dcd899233be019540a6e7591a6ee257a
KalinaVitaly <kalina.vitaly2016@yandex.ru>
2021-04-03 01:15:23 +0300

    Fix bugs

--- SecondLab/SecondLab/Source.cpp
+++ SecondLab/SecondLab/Source.cpp
@@ -3,15 +3,13 @@
 #include <string>
 #include <cmath>
 
-unsigned long long int EulerFunction(unsigned long long int p, unsigned long long int q);
-unsigned long long int MOD(unsigned long long int p, unsigned long long int q);
-unsigned long long int EFunction(unsigned long long int f);
-std::vector<bool> PrimeNumbers(unsigned long long int n);
-unsigned long long int EuclidAlgorithm(unsigned long long int a, unsigned long long int b);
-unsigned long long int EncryptionSymbol(unsigned long long int symbol, unsigned long long int e, unsigned long long int mod);
-//char ReEncryptionSymbol(long long int symbol, long long int d, long long int mod);
-unsigned long long int DFind(unsigned long long int f, unsigned long long int e);
-unsigned long long int ReEncryptionSymbol(unsigned long long int number, unsigned long long int power, unsigned long long int n)
+int EulerFunction(int p, int q);
+int MOD(int p, int q);
+int EFunction(int f);
+std::vector<bool> PrimeNumbers(int n);
+int EuclidAlgorithm(int a, int b);
+int DFind(int f, int e);
+int ReEncryptionSymbol(int number, int power, int n)
 {
 	unsigned long long res = 1;
 	while (power)
@@ -26,13 +24,13 @@ unsigned long long int ReEncryptionSymbol(unsigned long long int number, unsigne
 
 
 int main() {
-	unsigned long long int p = 3;
-	unsigned long long int q = 23;
-	unsigned long long int f = -1;
-	unsigned long long int mod = -1;
-	unsigned long long int e = -1;
-	unsigned long long int d = -1;
-	unsigned long long int data = 98;
+	int p = 7;
+	int q = 131;
+	int f = -1;
+	int mod = -1;
+	int e = -1;
+	int d = -1;
+	int data = 98;
 	std::string data_number;
 	std::string reEncriptionData = "";
 	std::vector<int> encriptionDataCode;
@@ -41,55 +39,42 @@ int main() {
 	f = EulerFunction(p, q);
 	e = EFunction(f);
 
-	//std::cin >> data;
 	std::cout << data << std::endl;
-	//std::cout << "Code: " << (int)data.data()[0] << std::endl;
-	//std::cout << data << std::endl;
-	//std::cout << "p: " << p << " q: " << q << " f: " << f << " mod: " << mod << " e: " << e << std::endl;
-	//std::cout << "Encription code:" << std::endl;
-	//for (int i = 0; i < data.size(); ++i) {
-	//	encriptionDataCode.push_back(ReEncryptionSymbol((int)data.data()[i], e, mod));
-	//	//std::cout << "EncryptionSymbol: " <<  EncryptionSymbol(static_cast<char>(data[i]), e, mod) << " ";
-	//}
-	//std::cout << std::endl;
-	std::cout << "Encryption: " << (data = ReEncryptionSymbol(data, e, mod)) << std::endl;
+
 	d = DFind(f, e);
-	//std::cout << data_number << std::endl;
-	/*for (auto i : encriptionDataCode) {
-		char a = (char)ReEncryptionSymbol(i, d = 29, mod = 91);
-		reEncriptionData += a;
-		std::cout << "Code: " << (int)a;
-	}*/
-	//std::cout << reEncriptionData << std::endl;
+	std::cout << "E: " << e << std::endl;
+	std::cout << "D: " << d << std::endl;
+	data = ReEncryptionSymbol(data, e, mod);
+	std::cout << "Encryption: " << data << std::endl;
 	std::cout << "Reencryption: " << ReEncryptionSymbol(data, d, mod) << std::endl;
-	//std::cout << "p: " << p << " q: " << q << " f: " << f << " mod: " << mod << " e: " << e << " d: " << d << std::endl;
-	//std::cout << "Reencryption number: " << ReEncryptionSymbol(data_number, d, mod);
+	
 	return 0;
 }
 
-unsigned long long int EulerFunction(unsigned long long int p, unsigned long long int q) {
+int EulerFunction(int p, int q) {
 	return (p - 1) * (q - 1);
 }
 
-unsigned long long int MOD(unsigned long long int p, unsigned long long int q) {
+int MOD(int p, int q) {
 	return p * q;
 }
 
-unsigned long long int EFunction(unsigned long long int f) {
+int EFunction(int f) {
 	std::vector<bool> numbers = PrimeNumbers(f);
-	int e = 1;
+	int e = 2;
 
-	do {
-		while (!numbers[e]) {
-			++e;
+	for (e = 2; e < f; e++)
+	{
+		if (EuclidAlgorithm(f, e) == 1)
+		{
+			return e;
 		}
-		
-	} while (EuclidAlgorithm(f, e) != 1);
+	}
 
-	return e;
+	return -1;
 }
 
-unsigned long long int EuclidAlgorithm(unsigned long long int a, unsigned long long int b) {
+int EuclidAlgorithm(int a, int b) {
 	while (a != b) {
 		if (a > b) {
 			a -= b;
@@ -100,7 +85,7 @@ unsigned long long int EuclidAlgorithm(unsigned long long int a, unsigned long l
 	return a;
 }
 
-std::vector<bool> PrimeNumbers(unsigned long long int n) {
+std::vector<bool> PrimeNumbers(int n) {
 	std::vector<bool> numbers(n + 1);
 	std::fill(numbers.begin(), numbers.end(), true);
 
@@ -114,21 +99,14 @@ std::vector<bool> PrimeNumbers(unsigned long long int n) {
 	return numbers;
 }
 
-unsigned long long int EncryptionSymbol(unsigned long long int symbol, unsigned long long int e, unsigned long long int mod) {
-	long long int up = static_cast<long long int>(pow(static_cast<long long int>(symbol), e));
-	//std::cout << "Up: " << up << std::endl;
-	return up % mod;
-}
-
-unsigned long long int DFind(unsigned long long int f, unsigned long long int e) {
-	double d = 0;
-	double intPart;
-	int k = 0;
+int DFind(int f, int e) {
+	int d = 0;
 	
-	do {
-		++k;
-		d = static_cast<double>(k * f + 1) / e;
-	} while (std::modf(d, &intPart) != 0);
+	while (d * e % f != 1)
+	{
+		//std::cout << "D: " << d << " E: " << e << " F: " << f << " Result: " << d * e % f << std::endl;
+		++d;
+	}
 
 	return d;
 }
